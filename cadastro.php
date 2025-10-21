6f23ae49329944a9b99242a4320697024ab3bf21
Lanna <lannakamillyfresmota@gmail.com>
2025-10-21 01:44:16 -0300

    oi

--- cadastro.php
+++ cadastro.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="shortcut icon" href="./imagens/GeoSync-bg.png" type="image/x-icon">
-       <title>Geosync</title>
+       <title>Geosync-cadastro</title>
     <style>
         body {
             box-sizing: border-box;
